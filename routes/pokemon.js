7349f183816ca740ae084247b9d1b702ad0ae3b6
billymoroney <YOUR-EMAIL-ADDRESS>
2020-10-26 16:07:01 -0500

    all deliverable routes working, just need styling now

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models')
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(pokemon=>{
+    res.render('pokemon/index', {pokemon: pokemon});
+  })  
 });
+
+router.get('/:id', function(req, res) {
+  //call pokemon api to get information on pokemon with this id
+  const pokemonURL = `http://pokeapi.co/api/v2/pokemon/${req.params.id}`
+  axios.get(pokemonURL).then(function(apiResponse){
+    const apiResults = apiResponse.data
+    res.send(apiResults)
+  })
+})
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(([pokemon, created])=>{
+    res.redirect('/pokemon')
+  })
 });
 
 module.exports = router;
